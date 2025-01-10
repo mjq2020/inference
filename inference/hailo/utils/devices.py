fa2610f7df355b05f9bd7a03e0e9ae5cc5b61173
mjq2020 <mjqx2011@163.com>
2025-01-10 08:39:55 +0000

    add: hailo device detect

--- inference/hailo/utils/devices.py
+++ inference/hailo/utils/devices.py
@@ -26,7 +26,17 @@ HAILO_DEVICES_DICT = dict(
 DEFAULT_HW_ARCH = "hailo8"
 
 
-def extract_board_config(board_config)->list:
+def extract_board_config(board_config) -> list:
+    """
+    Extracts and returns a list of available Hailo devices present in the given board configuration.
+
+    Args:
+        board_config (str): A string containing the board configuration details.
+
+    Returns:
+        list: A list of strings representing the available Hailo devices identified in the board configuration.
+    """
+
     avable_device = []
     for device, device_config in HAILO_DEVICES_DICT.items():
         if device_config in board_config:
@@ -35,7 +45,21 @@ def extract_board_config(board_config)->list:
     return avable_device
 
 
-def get_hailo_devices()->list:
+def get_hailo_devices() -> list:
+    """
+    Returns a list of available Hailo devices on the system.
+
+    The function first checks if the Hailo SDK is installed. If not, it logs a warning message and returns an empty list.
+
+    It then attempts to get the physical devices using the Hailo SDK. If an exception occurs during this step, it logs a warning message and returns an empty list.
+
+    For each physical device, it reads the board configuration and extracts the available Hailo devices using the `extract_board_config` function. If an exception occurs during this step, it logs a warning message and continues to the next device.
+
+    Finally, it returns the list of available Hailo devices.
+
+    Returns:
+        list: A list of strings representing the available Hailo devices on the system.
+    """
     try:
         from hailo_platform import VDevice
     except ImportError:
@@ -48,7 +72,7 @@ def get_hailo_devices()->list:
     except Exception as e:
         logger.warning(f"Error getting hailo devices: {e}")
         return hailo_available_devices
-    
+
     for device in physical_devices:
         try:
             board_config = str(device.control.read_board_config())
@@ -62,18 +86,70 @@ def get_hailo_devices()->list:
     return hailo_available_devices
 
 
-def get_all_devices()->list:
+def get_all_devices() -> list:
     # get onnx available devices
+    """
+    Retrieves a list of all available devices for inference.
+
+    It combines available devices from both ONNX and Hailo platforms.
+    The function first obtains the list of available ONNX providers.
+    Then, it retrieves the available Hailo devices using the `get_hailo_devices` function.
+
+    Returns:
+        list: A combined list of strings representing the available devices on both ONNX and Hailo platforms.
+    """
+
     onnx_available_devices = C.get_available_providers()
 
     # get hailo available devices
     hailo_available_devices = get_hailo_devices()
     return onnx_available_devices + hailo_available_devices
 
-def check_hailo_device()->bool:
-    output = subprocess.run("lspci",capture_output=True, check=True).stdout.decode()
-    
+
+def hailort_avilable() -> bool:
+    """
+    Checks if Hailo RT is available on the system.
+
+    Returns True if Hailo devices are present and the Hailo SDK is installed, False otherwise.
+    """
+    if get_hailo_devices():
+        try:
+            import hailo_platform
+
+            return True
+        except ImportError:
+            return False
+    return False
+
+
+def get_optimal_providen() -> str:
+    """
+    Retrieves the optimal provider for performing inference.
+
+    The function checks if Hailo devices are present and the Hailo SDK is installed. If so, it returns the first
+    available Hailo device. Otherwise, it returns the first available provider from the list of available ONNX providers.
+
+    Returns:
+        str: The optimal provider for inference.
+    """
+    hailo_devices = get_hailo_devices()
+    if hailo_devices and hailort_avilable():
+        return hailo_devices[0]
+    onnx_available_devices = C.get_available_providers()
+    return onnx_available_devices[0]
+
+
+def check_hailo_device() -> bool:
+    """
+    Checks if a Hailo device is present in the system.
+
+    Returns:
+        bool: True if a Hailo device is present, False otherwise.
+    """
+    output = subprocess.run("lspci", capture_output=True, check=True).stdout.decode()
+
     return "Hailo" in output
+
 
 if __name__ == "__main__":
     print(check_hailo_device())
